4e2df133c6c55c6bd21fab0cb923f6ad98bfede6
Sergio Silva <sergiobtos@hotmail.com>
2020-10-29 16:16:59 -0400

    29/10/2020 4:16pm

--- src/main/java/ca/ibs/imenu/controller/HomeController.java
+++ src/main/java/ca/ibs/imenu/controller/HomeController.java
@@ -1,5 +1,9 @@
 package ca.ibs.imenu.controller;
 
+import ca.ibs.imenu.entity.Role;
+import ca.ibs.imenu.entity.User;
+import ca.ibs.imenu.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/")
     public String defaultIndex(Model model) {
         model.addAttribute("name","World");
@@ -17,6 +24,34 @@ public class HomeController {
     public String index(Model model, @RequestParam(name = "name",defaultValue = "Caique") String name) {
         model.addAttribute("name",name);
         return "index";
+    }
+
+    @RequestMapping(value = "/viewLogin", method = RequestMethod.GET)
+    public String viewLogin() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(Model model, String username, String password) {
+        User user = userService.findByUsername(username);
+        if (user!=null && user.getPassword().equals(password)){
+
+            model.addAttribute("body",(user.getRole().equals(Role.Administrator)?"adminPanel":"staffPanel")+".jsp");
+            model.addAttribute("object",user);
+            model.addAttribute("title", "Panel of "+(user.getRole().equals(Role.Administrator)?"Administrator":"Staff"));
+
+            return "adminTemplate";
+        } else {
+            model.addAttribute("msgErro","Username/Password is invalid");
+        }
+        return "login";
+    }
+
+    @RequestMapping(value = "/adminPanel", method = RequestMethod.GET)
+    public String adminPanel(Model model) {
+        model.addAttribute("body","adminPanel.jsp");
+        model.addAttribute("title", "Panel of Administrator");
+        return "adminTemplate";
     }
 
 
---
+++ src/main/webapp/view/staffPanel.jsp
@@ -0,0 +1,8 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<a href="/listProduct">List of Products</a>
+<a href="/listOrder">List of Orders</a>
+<a href="/listOrderByTableNumber">List Order By Table Number</a>
+<a href="/changeTableNumber">Change Table Number</a>
+
