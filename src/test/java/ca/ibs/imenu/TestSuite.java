0adbeee71f9fcff962f2dc5bfd5e541060bedbd7
Caique Ferreira <caiquebferreira@gmail.com>
2020-11-20 12:03:41 -0500

    JUnit Test

--- src/test/java/ca/ibs/imenu/TestSuite.java
+++ src/test/java/ca/ibs/imenu/TestSuite.java
@@ -3,6 +3,8 @@ package ca.ibs.imenu;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(Suite.class)
 @SuiteClasses({UserTest.class})
--- src/test/java/ca/ibs/imenu/UserTest.java
+++ src/test/java/ca/ibs/imenu/UserTest.java
@@ -1,15 +1,36 @@
 package ca.ibs.imenu;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ca.ibs.imenu.entity.Role;
+import ca.ibs.imenu.entity.User;
+import ca.ibs.imenu.service.UserService;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@TestPropertySource(value = "classpath:testApplication.yml")
 public class UserTest {
-
+	
+	@Autowired
+	private UserService userService;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -17,18 +38,39 @@ public class UserTest {
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
-
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setup() {
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-
+		
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void case001_findAll() throws Exception {
+		List<User> users = userService.findAll();	
+		//Expected - Actual
+		assertEquals("admin",users.get(0).getUsername());
+		assertEquals(Role.ADMINISTRATOR,users.get(0).getRole());
+	}
+
+	
+	@Test
+	public void case002_addUser() throws Exception {
+		User user = new User(); 
+		user.setName("staff");
+		user.setRole(Role.STAFF);
+		user.setPassword("1");
+		user.setUsername("staff");
+				
+		user = userService.save(user);
+		User userDB = userService.findByUsername(user.getUsername());
+		
+		//Expected - Actual
+		assertEquals(user.getName(), userDB.getName());
+		assertEquals(user.getRole(), userDB.getRole());
+		assertEquals(user.getUsername(), userDB.getUsername());
 	}
 
 }
