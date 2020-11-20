f48eab9ebb7ab36a0bb5e8804c55a1124282aebe
Caique Ferreira <caiquebferreira@gmail.com>
2020-11-20 10:30:50 -0500

    Setup test cases

---
+++ src/test/java/ca/ibs/imenu/TestSuite.java
@@ -0,0 +1,11 @@
+package ca.ibs.imenu;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({UserTest.class})
+public class TestSuite {
+
+}
