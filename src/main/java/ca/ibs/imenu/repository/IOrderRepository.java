e1dbb93204589a8241ded926492b6b4092f6123f
Caique Ferreira <caiquebferreira@gmail.com>
2021-04-09 20:52:48 -0400

    Changes

--- src/main/java/ca/ibs/imenu/repository/IOrderRepository.java
+++ src/main/java/ca/ibs/imenu/repository/IOrderRepository.java
@@ -11,16 +11,40 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
+
+/**
+ * IOrderRepository - repository used to persist, merge, delete and search orders
+ * Date 2020-12-04
+ *
+ * @author Caique
+ * @version 0.0.1
+ */
 @Repository
 public interface IOrderRepository
         extends JpaRepository<Order, Long>, PagingAndSortingRepository<Order, Long>, JpaSpecificationExecutor<IOrderRepository> {
 
+	/**
+	 * findByStatusAndTableNumber - find order which have the filtered status and table number
+	  * Date 2020-12-04
+	 *
+	 * @param status  - table number which will be considered in the query
+	 * @param tableNumber - table number which will be considered in the query
+	 * @return Order 
+	 */
     @Query(value = "Select o " +
             "FROM Order o " +
             "WHERE o.status <> :#{#status}" +
             "   AND o.tableNumber = :#{#tableNumber}  ")
     Order findByStatusAndTableNumber(@Param("status") Status status,@Param("tableNumber") int tableNumber);
 
+    /**
+	 * myFindAll - list of orders which have the filtered status
+	 * Date 2020-12-04
+	 *
+	 * @param status  - table number which will be considered in the query
+	 * @return Order 
+	 */
     @Query(value = "Select o " +
             "FROM Order o " +
             "WHERE o.status <> :#{#status}")
